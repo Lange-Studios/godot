75d7c256bb80a46b3f5bc8f7c3c2d8a90230b4ad
Travis Lange <travislange12@gmail.com>
2024-05-09 08:40:19 -0400

    allows more control over build tools used during compilation process

--- platform/windows/detect.py
+++ platform/windows/detect.py
@@ -256,6 +256,10 @@ def build_res_file(target, source, env: "SConsEnvironment"):
 
     mingw_bin_prefix = get_mingw_bin_prefix(env["mingw_prefix"], env["arch"])
 
+    if not env["platform_tools"]:
+        cmdbase = env["WINDRES"]
+        mingw_bin_prefix = ""
+
     for x in range(len(source)):
         ok = True
         # Try prefixed executable (MinGW on Linux).
@@ -772,27 +776,28 @@ def configure_mingw(env: "SConsEnvironment"):
 
     mingw_bin_prefix = get_mingw_bin_prefix(env["mingw_prefix"], env["arch"])
 
-    if env["use_llvm"]:
-        env["CC"] = mingw_bin_prefix + "clang"
-        env["CXX"] = mingw_bin_prefix + "clang++"
-        if try_cmd("as --version", env["mingw_prefix"], env["arch"]):
-            env["AS"] = mingw_bin_prefix + "as"
-            env.Append(ASFLAGS=["-c"])
-        if try_cmd("ar --version", env["mingw_prefix"], env["arch"]):
-            env["AR"] = mingw_bin_prefix + "ar"
-        if try_cmd("ranlib --version", env["mingw_prefix"], env["arch"]):
-            env["RANLIB"] = mingw_bin_prefix + "ranlib"
-        env.extra_suffix = ".llvm" + env.extra_suffix
-    else:
-        env["CC"] = mingw_bin_prefix + "gcc"
-        env["CXX"] = mingw_bin_prefix + "g++"
-        if try_cmd("as --version", env["mingw_prefix"], env["arch"]):
-            env["AS"] = mingw_bin_prefix + "as"
-        ar = "ar" if os.name == "nt" else "gcc-ar"
-        if try_cmd(f"{ar} --version", env["mingw_prefix"], env["arch"]):
-            env["AR"] = mingw_bin_prefix + ar
-        if try_cmd("gcc-ranlib --version", env["mingw_prefix"], env["arch"]):
-            env["RANLIB"] = mingw_bin_prefix + "gcc-ranlib"
+    if env["platform_tools"]:
+        if env["use_llvm"]:
+            env["CC"] = mingw_bin_prefix + "clang"
+            env["CXX"] = mingw_bin_prefix + "clang++"
+            if try_cmd("as --version", env["mingw_prefix"], env["arch"]):
+                env["AS"] = mingw_bin_prefix + "as"
+                env.Append(ASFLAGS=["-c"])
+            if try_cmd("ar --version", env["mingw_prefix"], env["arch"]):
+                env["AR"] = mingw_bin_prefix + "ar"
+            if try_cmd("ranlib --version", env["mingw_prefix"], env["arch"]):
+                env["RANLIB"] = mingw_bin_prefix + "ranlib"
+            env.extra_suffix = ".llvm" + env.extra_suffix
+        else:
+            env["CC"] = mingw_bin_prefix + "gcc"
+            env["CXX"] = mingw_bin_prefix + "g++"
+            if try_cmd("as --version", env["mingw_prefix"], env["arch"]):
+                env["AS"] = mingw_bin_prefix + "as"
+            ar = "ar" if os.name == "nt" else "gcc-ar"
+            if try_cmd(f"{ar} --version", env["mingw_prefix"], env["arch"]):
+                env["AR"] = mingw_bin_prefix + ar
+            if try_cmd("gcc-ranlib --version", env["mingw_prefix"], env["arch"]):
+                env["RANLIB"] = mingw_bin_prefix + "gcc-ranlib"
 
     ## LTO
 
