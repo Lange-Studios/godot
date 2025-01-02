d14b9b7fe62350a5942079b6c29f31dded54816a
Travis Lange <travislange12@gmail.com>
2025-01-02 11:29:11 -0500

    Revert "Merge branch 'manually-specify-build-tools' into master-lange-studios"
    This reverts commit ea0b4446a88835e24cb4f12561ce3367dbb4c068, reversing
    changes made to e8307eb6b638cc9b8f5848086c6b552cae11b812.

--- platform/windows/detect.py
+++ platform/windows/detect.py
@@ -736,6 +736,15 @@ def configure_mingw(env: "SConsEnvironment"):
     env.Append(CCFLAGS=["-ffp-contract=off"])
 
     if env["use_llvm"]:
+        env["CC"] = mingw_bin_prefix + "clang"
+        env["CXX"] = mingw_bin_prefix + "clang++"
+        if try_cmd("as --version", env["mingw_prefix"], env["arch"]):
+            env["AS"] = mingw_bin_prefix + "as"
+            env.Append(ASFLAGS=["-c"])
+        if try_cmd("ar --version", env["mingw_prefix"], env["arch"]):
+            env["AR"] = mingw_bin_prefix + "ar"
+        if try_cmd("ranlib --version", env["mingw_prefix"], env["arch"]):
+            env["RANLIB"] = mingw_bin_prefix + "ranlib"
         env.extra_suffix = ".llvm" + env.extra_suffix
 
     if env["platform_tools"]:
